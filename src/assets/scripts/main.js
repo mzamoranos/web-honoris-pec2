c9d2653001bf1b87b674f43b7eb5e87c41214db6
Mónica <51133992+mzamoranos@users.noreply.github.com>
2022-12-07 18:14:06 +0100

    Add files via upload

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,8 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+import "npm:@fortawesome/fontawesome-free/css/all.css";
+/**
+ * Write any other JavaScript below
+ */
